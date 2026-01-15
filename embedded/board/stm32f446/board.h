3d2204708873155584b51665e347b8c7e246e062
Emmortyy7 <halisvurgunemre@gmail.com>
2026-01-15 09:15:22 +0300

    commit

--- embedded/board/stm32f446/board.h
+++ embedded/board/stm32f446/board.h
@@ -1,8 +1,12 @@
-#ifndef BOARD_H
-#define BOARD_H
+/*
+
+   All includes and some settings gathered in one file for app/main.c so it's gonna be easier for writing main logic.     
+
+*/
+
+
+#pragma once
 
 #include "stm32f446xx.h"
 
 void board_init(void);
-
-#endif
