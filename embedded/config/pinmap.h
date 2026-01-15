5af0094eb3a0700057676024902ea20147d0417a
Emmortyy7 <halisvurgunemre@gmail.com>
2026-01-15 23:07:22 +0300

    com

--- embedded/config/pinmap.h
+++ embedded/config/pinmap.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include "board.h"
+
 #ifdef F446_MCU
 
 /* Serial Port pin.  */
 #define UART_CMD_PIN__UART1       GPIO_PIN_9
 #define UART_CMD_PIN_GROUP__UART1 GPIOA
+
+#define ADC_PIN__ADC1       GPIO_PIN_0
+#define ADC_PIN_GROUP__ADC1 GPIOA
 
 #endif
